28dad52963f3767797d2149fdfad1f106495bc0b
helloworld0627 <helloworld0627@gmail.com>
2016-02-24 20:24:38 -0800

    Update Main.scala

--- week1/Main.scala
+++ week1/Main.scala
@@ -14,34 +14,31 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int =
-    if (r < 0 || c < 0 || c > r+1) 0;
-    else if (r == 0 && c == 0) 1;
-    else pascal (c, r-1) + pascal(c-1, r-1);
+  def pascal(c: Int, r: Int): Int = {
+    if (r == 0 && c == 0) 1
+    else if (c > r || r < 0) 0
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
   def balance(chars: List[Char]): Boolean = {
-    def inner_balance(chars: List[Char], count: Int) : Boolean = {
-      if (chars.isEmpty && count == 0) true;
-      else if (chars.isEmpty && count != 0) false;
-      else if (count < 0) false;
-      else if (chars.head == ')') inner_balance(chars.tail, (count-1));
-	  else if (chars.head == '(') inner_balance(chars.tail, (count+1));
-	  else inner_balance(chars.tail, count);
-  	}
-    inner_balance (chars, 0);
+    def count(chars: List[Char], current: Int): Int =
+      if (chars.isEmpty || current < 0) current
+      else if (chars.head == '(') count(chars.tail, current + 1)
+      else if (chars.head == ')') count(chars.tail, current - 1)
+      else count(chars.tail, current)
+    count(chars, 0) == 0
   }
 
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int =
-    if (money == 0) 1;
-    else if (money < 0) 0;
-    else if (money > 0 && coins.isEmpty) 0;
-    else countChange(money - coins.head, coins) + countChange(money, coins.tail);
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
-
